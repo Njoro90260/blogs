54e6917fc734128534fd1f083e8b91f8adc71f22
peter <wchegesalome@gmail.com>
2024-09-01 12:31:40 -0400

    home-page

---
+++ blogs/urls.py
@@ -0,0 +1,10 @@
+"""Defines url patterns for blogs."""
+from django.urls import path
+from . import views
+
+app_name = 'blogs'
+urlpatterns = [
+    #Home page
+    path('', views.index, name='index'),
+    path('blogs/', views.blogs name="blogs"),
+]
