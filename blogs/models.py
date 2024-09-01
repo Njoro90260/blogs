00c575e63e9f9c151bee897514ba04a6b90e473d
peter <wchegesalome@gmail.com>
2024-09-01 11:17:48 -0400

    models

---
+++ blogs/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class BlogPost(models.Model):
+    """Model for the Blog Post."""
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return f"{self.title}: {self.text[:50]}..."
+
+
