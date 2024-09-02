c3724d4abaf7bbac08c17ac7bb94e3b94a9d2cd3
peter <wchegesalome@gmail.com>
2024-09-02 09:37:53 -0400

    view blog

--- blogs/urls.py
+++ blogs/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     #Home page
     path('', views.index, name='index'),
     path('blogs/', views.blogs, name="blogs"),
+    path('blog-view/<int:title_id>', views.blogpost, name="blogpost"),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import BlogPost
+from django.urls import reverse
 # Create your views here.
 
 def index(request):
@@ -10,3 +11,10 @@ def blogs(request):
     blogs = BlogPost.objects.order_by('date_added')
     context = {'blogs': blogs}
     return render(request, 'blogs/blogs.html', context)
+
+def blogpost(request, title_id):
+    blogpost = BlogPost.objects.get(id=title_id)
+    context = {'blogpost': blogpost}
+    return render(request, 'blogs/blogpost.html', context)
+
+
