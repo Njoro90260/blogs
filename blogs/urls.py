3fb61ce4a35891aca69a93c535a78b4c05168d40
peter <wchegesalome@gmail.com>
2024-09-04 00:32:23 -0400

    view blog.1

--- blogs/urls.py
+++ blogs/urls.py
@@ -6,6 +6,10 @@ app_name = 'blogs'
 urlpatterns = [
     #Home page
     path('', views.index, name='index'),
+    #path to view all the blogs
     path('blogs/', views.blogs, name="blogs"),
+    #path to a specific blog
     path('blog-view/<int:title_id>', views.blogpost, name="blogpost"),
+    # path to add a new blog.
+    path('new-blog/', views.newblog, name="newblog"),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import BlogPost
 from django.urls import reverse
+from .forms import BlogForm
+from django.urls import reverse 
 # Create your views here.
 
 def index(request):
@@ -8,13 +10,33 @@ def index(request):
     return render(request, 'blogs/index.html')
 
 def blogs(request):
+    """Page for the blogs."""
     blogs = BlogPost.objects.order_by('date_added')
     context = {'blogs': blogs}
     return render(request, 'blogs/blogs.html', context)
 
 def blogpost(request, title_id):
+    """Page for the blogpost."""
     blogpost = BlogPost.objects.get(id=title_id)
     context = {'blogpost': blogpost}
     return render(request, 'blogs/blogpost.html', context)
+
+def newblog(request):
+    """Page for adding a new blog."""
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = BlogForm()
+    else:
+        # POST data submitted; process data.
+        form = BlogForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('blogs:blogs', args=[newblog.id]))
+        
+        # Display a blank or invalid form.
+        context = {'form': form}
+        return render(request, 'blogs/new_blog.html', context)
+
+
 
 
