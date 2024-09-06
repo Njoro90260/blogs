5e0d67ef7630d3704b8acaae75ec500c73c697ad
peter <wchegesalome@gmail.com>
2024-09-06 12:24:00 -0400

    add app users

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+"""Defines urls patterns for users"""
+from django.urls import path, include
+
+app_name = 'users'
+urlpatterns = [
+    # Include default auth urls.
+    path('', include('django.contrib.auth.urls')),
+]
